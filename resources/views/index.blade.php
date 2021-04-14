96459b65605a634a51a61c3ca63b4ad012fa2d86
Pietto <Pieterjan.v.dijk@gmail.com>
2021-04-14 16:34:08 +0200

    css slowly taking shape, staff members in progress

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -33,27 +33,96 @@
         <h1>FEATURING</h1>
         <h2>Most popular items:</h2>
         <div id='most_popular_item_1' class='most_popular_items'><div class='most_popular_items_cover'>
-            <h3>"cool train!"</h3>
-            <p>'subtext'</p>
+            <h3>ns V-IRM(m)</h3>
+            <p>'snel en efficiënt'</p>
         </div></div>
         <div id='most_popular_item_2' class='most_popular_items'><div class='most_popular_items_cover'>
-            <h3>"cool train!"</h3>
-            <p>'subtext'</p>
+            <h3>ns Mat '64</h3>
+            <p>'oude klassieker'</p>
         </div></div>
         <div id='most_popular_item_3' class='most_popular_items'><div class='most_popular_items_cover'>
-            <h3>"cool train!"</h3>
-            <p>'subtext'</p>
+            <h3>ns ICM(m)</h3>
+            <p>'snel en comfortabel'</p>
         </div></div>
         <div id='most_popular_item_4' class='most_popular_items'><div class='most_popular_items_cover'>
-            <h3>"cool train!"</h3>
-            <p>'subtext'</p>
+            <h3>ns e-loc 1700</h3>
+            <p>'sterke loc voor allerlei doeleinden'</p>
         </div></div>
     </div>
-    <button id='view_all_products_button' onclick="alert('This part hasn\'t been coded in yet')">VIEW ALL PRODUCTS</button>
 
-    <div class='btn btn-one' id='view_all_products_button'>
-        <span>HOVER ME</span>
-</div>
+    <div class='btn btn-animation-one animated_buttons' id='view_all_products_button' onclick="alert('This part hasn\'t been coded in yet')">
+        <span>BEKIJK ALLE PRODUCTEN</span>
+    </div>
+
+    <div id='popular_categories_wrapper'>
+        <div class='popular_categories' id='popular_category_1'>
+            <div class='btn btn-animation-three animated_buttons' id='view_all_emus_button' onclick="alert('This part hasn\'t been coded in yet')">
+                <span>bekijk emu's</span>
+            </div>
+
+            <div class='popular_items_cover'>
+                <h1>Electric Multiple Units</h1>
+                <p>Efficiënt en gemakkelijk in gebruik, dit type trein is zeer populair, en met goede rede.</p>
+            </div>
+        </div>
+
+        <div class='popular_categories' id='popular_category_2'>
+            <div class='btn btn-animation-three animated_buttons' id='view_all_classics_button' onclick="alert('This part hasn\'t been coded in yet')">
+                <span>bekijk de klassiekers!</span>
+            </div>
+
+            <div class='popular_items_cover'>
+                <h1>Klassiekers</h1>
+                <p>Dit zijn de klassiekers van het Nederlandse spoor! ze hebben hun dienstjaren erop zitten, maar ze hebben hun betrouwbaarheid bewezen in hun dienstjaren.</p>
+            </div>
+        </div>
+
+        <div class='popular_categories' id='popular_category_3'>
+            <div class='btn btn-animation-three animated_buttons' id='view_all_internationals_button' onclick="alert('This part hasn\'t been coded in yet')">
+                <span>bekijk de internationals!</span>
+            </div>
+
+            <div class='popular_items_cover'>
+                <h1>Internationaal</h1>
+                <p>Dit zijn de treinen die ook over de grens kunnen rijden, zonder functionaliteit of betrouwbaarheid op te hoeven offeren.</p>
+            </div>
+        </div>
+    </div>
+
+    <div id='info_wrapper'>
+        <div id='about_us_text' class='info_text_wrapper'>
+            <h1>OVER ONS</h1>
+            <h2>RANDOM TEXT</h2>
+            <p>My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there. Then I'm gonna shoot that bitch in the kneecaps, find out where my goddamn money is. She gonna tell me too. Hey, look at me when I'm talking to you, motherfucker. You listen: we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand? </p>
+        </div>
+        <div id='about_us_img' class='info_img_wrapper'></div>
+
+        <div id='location_img' class='info_img_wrapper'></div>
+        <div id='location_text' class='info_text_wrapper'>
+            <h1>NOG WAT INFO</h1>
+            <h2>WAAR ZIJN WIJ WERKZAAM</h2>
+            <p>My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there. Then I'm gonna shoot that bitch in the kneecaps, find out where my goddamn money is. She gonna tell me too. Hey, look at me when I'm talking to you, motherfucker. You listen: we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand? </p>
+        </div>
+
+        <div id='satisfaction_text' class='info_text_wrapper'>
+            <h1>WAT VINDEN JULLIE VAN ONS</h1>
+            <h2>KLANT TEVREDENHEID</h2>
+            <p>My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there. Then I'm gonna shoot that bitch in the kneecaps, find out where my goddamn money is. She gonna tell me too. Hey, look at me when I'm talking to you, motherfucker. You listen: we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand? </p>
+        </div>
+        <div id='satisfaction_img' class='info_img_wrapper'></div>
+    </div>
+
+    <div id='staff_wrapper'>
+        <h1>Teamleden</h2>
+        <h2>BESTE TEAM OOIT</h2>
+        <div id='staff_img_wrapper'>
+            <div class='staff_members' id='staff_member_1'></div>
+            <div class='staff_members' id='staff_member_2'></div>
+            <div class='staff_members' id='staff_member_3'></div>
+            <div class='staff_members' id='staff_member_4'></div>
+            <div class='staff_members' id='staff_member_5'></div>
+        </div>
+    </div>
 
 
 

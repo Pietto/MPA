927b8c001b6f4ad2d061b588640212cb9e8a59cd
Pietto <Pieterjan.v.dijk@gmail.com>
2021-05-08 14:16:22 +0200

    slight development with the login logic

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../resources/css/app.css">
-    <link rel="stylesheet" href="../resources/css/animations.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
+    <link rel="stylesheet" href="../resources/css/app.css">
+    <link rel="stylesheet" href="../resources/css/animations.css">
     <title>Train Shop</title>
 </head>
 <body>
@@ -24,19 +24,29 @@
         <div id='head_wrapper_center'>
             <img id='header_logo' alt='company logo' src='../images/logo/logo_plain.png'/>
         </div>
-            
         <div id='head_wrapper_right'>
             <a onclick='switchTheme()' class="btn2 btn-animation-four" id='themeSwitchButton' data-sm-link-text="Lightmode" target="_blank"><span>Mode</span></a>
             <a href='{{Route("product.shoppingCart")}}'>cart</a>
             <div class="w3-dropdown-click">
-            <a onclick="dropDown()" class="">account</a>
-            <div id="Demo" class="w3-dropdown-content w3-bar-block w3-card-4 w3-animate-zoom">
-                <a href="#" class="w3-bar-item w3-button">Login</a>
-                <a href="#" class="w3-bar-item w3-button">Register</a>
+                <a onclick="dropDown()" class="dropdownButton">account</a>
+                <div id="Demo" class="w3-dropdown-content w3-bar-block w3-card-4 w3-animate-opacity">
+                    @if (Route::has('login'))
+                        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                            @auth
+                                <a><?= Auth::user()->username ?></a>
+                                <a href="http://localhost/jaar%202/Laravel/project_A/MPA/public/logout" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="dropdown-item w3-bar-item">uitloggen</a>
+                                <form id="logout-form" action="http://localhost/jaar%202/Laravel/project_A/MPA/public/logout" method="POST" class="d-none"><input type="hidden" name="_token" value="sfhh5t43nRlFlqm3umHXxPeF1rQRzScFEwjJ6fE1"></form>
+                            @else
+                                <a href="{{ route('login') }}" class="dropdown-item w3-bar-item w3-button">Inloggen</a>
+                                @if (Route::has('register'))
+                                    <a href="{{ route('register') }}" class="dropdown-item w3-bar-item w3-button">Registreren</a>
+                                @endif
+                            @endauth
+                        </div>
+                    @endif
+                </div>
             </div>
-        </div>
-
-            <a>account</a>
+            <a></a><a></a>
         </div>
     </div>
 
@@ -47,7 +57,7 @@
             </div>
         </div>
     </a>
-    
+
     <div id='most_popular_showcase_wrapper'>
         <h1>FEATURING</h1>
         <h2>Most popular items:</h2>
@@ -134,7 +144,7 @@
     <? include '../resources/views/include/general/footer.php'; ?>
     <script src='../resources/js/ThemeSwitch.js'></script>
 
-    
+
     @if (Route::has('login'))
         <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
             @auth
@@ -154,16 +164,7 @@
 
 
 
-    <script>
-        function dropDown() {
-            var x = document.getElementById("Demo");
-            if (x.className.indexOf("w3-show") == -1) {
-                x.className += " w3-show";
-            } else { 
-                x.className = x.className.replace(" w3-show", "");
-            }
-        }
-    </script>
+    <script src='../resources/js/DropDownLogic.js'></script>
 
 </body>
 </html>
